46ab2fdf00eaef040af236b353a3e0bb1517370d
hyeneung <imasaru22@gmail.com>
2024-09-24 22:49:19 +0900

    #6 - fix: deserialize Elasticsearch result with @JsonProperty
    - Used Jackson's @JsonProperty for custom field mapping during deserialization
    - Configured @Document classes to match Elasticsearch response structure
    - Added dependency for Jackson library

--- backend/src/main/java/org/datacapstonedesign/backend/document/ArticleInfoDocument.java
+++ backend/src/main/java/org/datacapstonedesign/backend/document/ArticleInfoDocument.java
@@ -1,5 +1,6 @@
 package org.datacapstonedesign.backend.document;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import org.datacapstonedesign.backend.generated.dto.ArticleInfo;
 import org.springframework.data.annotation.Id;
@@ -8,24 +9,25 @@ import org.springframework.data.elasticsearch.annotations.Field;
 
 @Document(indexName = "#{@environment.getProperty('spring.elasticsearch.index.name')}")
 public record ArticleInfoDocument(
+    @JsonProperty("id")
     @Id
     String id,
-
+    @JsonProperty("title")
     String title,
-
+    @JsonProperty("pub_date")
     @Field(name = "pub_date")
     String pubDate,
-
+    @JsonProperty("company_name")
     @Field(name = "company_name")
     String companyName,
-
+    @JsonProperty("url")
     String url,
-
+    @JsonProperty("content")
     String content,
-
+    @JsonProperty("summarized_text")
     @Field(name = "summarized_text")
     String summarizedText,
-
+    @JsonProperty("hashtags")
     List<String> hashtags
 ) {
     public static ArticleInfo toArticleInfo(ArticleInfoDocument articleInfoDocument) {
