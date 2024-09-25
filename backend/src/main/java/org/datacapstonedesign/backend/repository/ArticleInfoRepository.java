8a413e74f90d48ea454c4e0802eea56bc2d29e16
hyeneung <imasaru22@gmail.com>
2024-09-25 11:42:04 +0900

    #6 - feat: Implement getRecommend API
    - Added getRecommend API endpoint
    - Implemented corresponding service method for retrieving recommendation and summarizedContents

--- backend/src/main/java/org/datacapstonedesign/backend/repository/ArticleInfoRepository.java
+++ backend/src/main/java/org/datacapstonedesign/backend/repository/ArticleInfoRepository.java
@@ -1,8 +1,9 @@
 package org.datacapstonedesign.backend.repository;
 
+import java.util.Optional;
 import org.datacapstonedesign.backend.document.ArticleInfoDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ArticleInfoRepository extends NativeQueryRepository, ElasticsearchRepository<ArticleInfoDocument, String>{
-
+    Optional<ArticleInfoDocument> findByUrl(String url);
 }
