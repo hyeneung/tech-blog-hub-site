e5fa71d671d6afeabc9236e3da34902837ba1fba
Chanhyeong Lee <dannychh@khu.ac.kr>
2024-10-01 22:01:04 +0900

    perf/fix: preprocessor 성능 개선 및 버그 수정
    - kakao, skplanet, oliveyoung, kurly, amazon 제외 나머지 사이트에 대해 모두 처리하도록 성능 개선
    - Title의 내용이 Subtitle에 중복되어 나오는 버그 수정

--- text_handler/utils/preprocess.py
+++ text_handler/utils/preprocess.py
@@ -34,22 +34,50 @@ def get_preprocessed_text(url: str) -> str:
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    headers = soup.find_all('header')
-    header = headers[1] if len(headers) > 1 else headers[0]
-    article = soup.article
 
-    preprocessed_text = ""
+    headers = soup.find_all('header')
+    header = None
+    if len(headers) == 0:
+        if soup.find('div', 'post-header') != None:
+            header = soup.find('div', 'post-header')
+        if soup.find('div', 'post-author-info') != None:
+            header = soup.find('div', 'post-author-info')
+        if soup.find('div', 'area_title') != None:
+            header = soup.find('div', 'area_title')
+    else:
+        header = headers[1] if len(headers) > 1 else headers[0]
+
+    article = soup.article
+    if article == None:
+        if soup.find('div', 'post') != None:
+            article = soup.find('div', 'post')
+        if soup.find('div', 'post-content-body') != None:
+            article = soup.find('div', 'post-content-body')
+        if soup.find('div', 'contents_style') != None:
+            article = soup.find('div', 'contents_style')
+        if soup.find('div', 'post_details') != None:
+            article = soup.find('div', 'post_details')
+
+    preprocessed_text = ''
 
     if article != None:
         preprocessed_text += 'Title: '
+        title = ''
         if header != None:
-            preprocessed_text += header.h1.get_text()
-        else:
-            preprocessed_text += article.h1.get_text()
-        preprocessed_text += '\n'
+            if header.h1 != None:
+                title = header.h1.get_text()
+            elif header.h3 != None:
+                title = header.h3.get_text()
+        if title == '':
+            if article.h1 != None:
+                title = article.h1.get_text()
+            else:
+                title = article.h2.get_text()
+        preprocessed_text += title + '\n'
         for tag in article.find_all(['h1', 'h2', 'h3', 'p']):
-            if tag.name in ['h1', 'h2']:
-                preprocessed_text += 'Subtitle: '
-            preprocessed_text += tag.get_text() + '\n'
+            if tag.get_text() != title:
+                if tag.name in ['h1', 'h2', 'h3']:
+                    preprocessed_text += 'Subtitle: '
+                preprocessed_text += tag.get_text() + '\n'
 
     return preprocessed_text
