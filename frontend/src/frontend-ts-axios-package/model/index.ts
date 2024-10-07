605cffb58d000252e1519dbdbe0c8a51c802e457
hyeneung <imasaru22@gmail.com>
2024-10-07 23:41:47 +0900

    #7 - feat: Add generated axios-typescript client code
    - Generate API client code using OpenAPI specification
    - Include TypeScript types for API requests and responses
    - Improve type safety and developer experience when interacting with the API

---
+++ frontend/src/frontend-ts-axios-package/model/index.ts
@@ -0,0 +1,7 @@
+export * from './api-result';
+export * from './article-info';
+export * from './page-info';
+export * from './recommend-response';
+export * from './recommend-response-body';
+export * from './search-response';
+export * from './search-response-body';
