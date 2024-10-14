cf32f69ce27f3b0fcc08f7cadbb4cff5b7df1d53
hyeneung <imasaru22@gmail.com>
2024-10-15 00:04:13 +0900

    #7 - Refactor article search logic and ensure reactive state updates
    - Remove duplicate fetchArticles() call in onMounted hook
    - Add immediate option to watch function for currentCriteria
    - Use $patch method in loadFromLocalStorage and resetCriteria for reactive updates
    - Ensure consistent article fetching on criteria changes

--- frontend/src/stores/articleSearchCriteriaStore.ts
+++ frontend/src/stores/articleSearchCriteriaStore.ts
@@ -11,19 +11,15 @@ export const useArticleSearchCriteriaStore = defineStore('articleSearchCriteria'
   actions: {
     setHashtags(hashtags: string[]){
         this.hashtags = hashtags
-        this.saveToLocalStorage()
     },
     setCompany(company: string){
         this.company = company
-        this.saveToLocalStorage()
     },
     setQuery(query: string) {
         this.query = query
-        this.saveToLocalStorage()
     },
     setPage(page: number){
         this.page = page
-        this.saveToLocalStorage()
     },
     saveToLocalStorage(expiryMinutes = 30) {
       const now = new Date();
@@ -42,29 +38,27 @@ export const useArticleSearchCriteriaStore = defineStore('articleSearchCriteria'
     loadFromLocalStorage() {
       const savedCriteria = localStorage.getItem('articleSearchCriteria')
       if (savedCriteria) {
-        const parsed = JSON.parse(savedCriteria);
-        const now = new Date().getTime();
-        
-        if (parsed.expiry && parsed.expiry > now) {
-          this.hashtags = parsed.value.hashtags;
-          this.company = parsed.value.company;
-          this.query = parsed.value.query;
-          this.page = parsed.value.page;
-          this.size = parsed.value.size;
-        } else {
-          this.resetCriteria();
-          localStorage.removeItem('articleSearchCriteria');
+        const { value, expiry } = JSON.parse(savedCriteria);
+        if (expiry && expiry > Date.now()) {
+          // If the saved search criteria is valid, update the state.
+          // This will trigger the watch function in PostList to load the data.
+          this.$patch(value);
+          return;
         }
-      } else{
-        this.resetCriteria()
       }
+      // If there are no saved criteria or they have expired, reset to default values.
+      // This change will trigger the watch function in PostList to load new data.
+      this.resetCriteriaAndLoad();
     },
-    resetCriteria() {
-        this.hashtags = []
-        this.company = ''
-        this.query = ''
-        this.page = 0
-        this.size = 10
+    resetCriteriaAndLoad() {
+      this.$patch({
+        hashtags: [],
+        company: '',
+        query: '',
+        page: 0,
+        size: 10
+      });
+      this.saveToLocalStorage();
     },
   },
   getters: {
