a989f50efcd38ebfca9d525d49c03263c4bd4506
Chanhyeong Lee <dannychh@khu.ac.kr>
2024-11-02 20:54:43 +0900

    chore: preprocessor selenium 옵션 설정
    - selenium 실행 시 창이 안 보이도록 옵션 설정

--- text_handler/utils/preprocess.py
+++ text_handler/utils/preprocess.py
@@ -2,6 +2,7 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -17,7 +18,12 @@ def get_preprocessed_text(url: str) -> str:
     Returns:
     str: 전처리된 텍스트
     """
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     time_to_live = 3
     while True:
         try:
