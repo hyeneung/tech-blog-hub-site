b273b58603af3cde0116e9a7a065eff58b01e190
hyeneung <imasaru22@gmail.com>
2024-11-06 06:19:03 +0900

    #7 - fix: Refactor selectedCompany to enable two-way binding with searchCriteriaStore
    - Updated setter to call setCompany on user selection.
    - Ensure UI reflects changes when search criteria are reset.

--- frontend/src/components/search/CompanySelect.vue
+++ frontend/src/components/search/CompanySelect.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="select-wrapper">
     <div class="custom-select">
-      <select v-model="selectedCompany" @change="updateSelectedCompany">
+      <select v-model="selectedCompany">
         <option value="">모든 회사</option>
         <option v-for="company in companies" :key="company.name" :value="company.name">
           {{ company.name }}
@@ -12,16 +12,21 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
 import { companies } from '@/data/companies'
 import { useArticleSearchCriteriaStore } from '@/stores/articleSearchCriteriaStore'
+import { storeToRefs } from 'pinia'
 
-const selectedCompany = ref('')
 const searchCriteriaStore = useArticleSearchCriteriaStore()
 
-const updateSelectedCompany = () => {
-  searchCriteriaStore.setCompany(selectedCompany.value)
-}
+const { company } = storeToRefs(searchCriteriaStore)
+// 양방향 바인딩
+// 사용자가 회사 선택 -> setter 호출. 검색조건 변경 -> api 호출
+// 사용자가 검색조건 초기화 선택 -> company 변경 -> 변경감지. getter 호출. selectedCompany 변경
+const selectedCompany = computed({
+  get: () => company.value,
+  set: (value) => searchCriteriaStore.setCompany(value)
+})
 </script>
 
 <style scoped>
